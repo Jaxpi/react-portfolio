f70980a54542ee3fe30379e67d0684384ea88b3a
Jax I <jaxpi85@gmail.com>
2022-12-05 09:08:50 -0500

    fixed display of projects to be evenly distributed and responsive to screen size change

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -5,19 +5,27 @@ const Portfolio = () => {
       <div class="portfolioimages">
         <div id="projects">
           <figure>
-            <figcaption>Boot Camp Study Guide</figcaption><a href="https://jaxpi.github.io/prework-study-guide/"><img src="./images/PWSG.png" alt="Screenshot of Study Guide" /></a>
+            <figcaption>Boot Camp Study Guide</figcaption><a href="https://jaxpi.github.io/prework-study-guide/"><img src="./images/PWSG.png" alt="Screenshot of Boot Camp Study Guide" /></a>
           </figure>
 
           <figure>
-            <figcaption>PWA Text Editor App</figcaption><a href="https://jaxpi-pwa-text-editor.herokuapp.com/"><img src="./images/Coming.png" alt="Screenshot of Code Refactor for Horiseon" /></a>
+            <figcaption>PWA Text Editor App</figcaption><a href="https://jaxpi-pwa-text-editor.herokuapp.com/"><img src="./images/jateinstappopen.jpg" alt="Screenshot of PWA Text Editor App" /></a>
           </figure>
 
           <figure>
-            <figcaption>Weather Dashboard</figcaption><a href="https://jaxpi.github.io/weather-dashboard/"><img src="./images/weathermainscreenshot.jpg" alt="Coming Soon - Stamp" /></a>
+            <figcaption>Weather Dashboard</figcaption><a href="https://jaxpi.github.io/weather-dashboard/"><img src="./images/weathermainscreenshot.jpg" alt="Screenshot of Weather Dashboard" /></a>
           </figure>
 
           <figure>
-            <figcaption>Work Day Scheduler</figcaption><a href="https://jaxpi.github.io/work-day-scheduler/"><img src="./images/workmainscreenshot.jpg" alt="Coming Soon - Construction" /></a>
+            <figcaption>Work Day Scheduler</figcaption><a href="https://jaxpi.github.io/work-day-scheduler/"><img src="./images/workmainscreenshot.jpg" alt="Screenshot of Work Day Scheduler" /></a>
+          </figure>
+
+          <figure>
+            <figcaption>Under Construction</figcaption><a href="#portfolio"><img src="./images/Construction.png" alt="Unnder Construction" /></a>
+          </figure>
+
+          <figure>
+            <figcaption>Coming Soon</figcaption><a href="#portfolio"><img src="./images/Coming.png" alt="Coming Soon" /></a>
           </figure>
         </div>
       </div>
