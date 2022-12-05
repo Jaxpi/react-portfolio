11e75b89044e9bb4caec1138c316258bfb3d858a
Jax I <jaxpi85@gmail.com>
2022-12-05 17:25:16 -0500

    deleted notes on app.js

--- src/App.js
+++ src/App.js
@@ -16,14 +16,3 @@ function App() {
 }
 
 export default App;
-
-//WHAT I NEED TO DO STILL:
-
-//HAVE LINK TO DEPLOYMENT AND GITHUB REPO FOR EACH PROJECT IN PORTFOLIO (MAYBE A MODAL POPUP ON EACH PIC WITH A BUTTON FOR EACH?)
-
-// WHEN I move my cursor out of one of the form fields without entering text THEN I receive a notification that this field is required
-// WHEN I enter text into the email address field THEN I receive a notification if I have entered an invalid email address
-
-//MAKE RESUME BUTTON FOR DOWNLOADING PDF VERSION OF RESUME WITH DUMMY PDF FOR UNDER CONSTRUCTION FOR NOW
-// WHEN I am presented with the Resume section
-// THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
