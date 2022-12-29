6922119d48a1a6d37af7d19fa2d0734e177a2abf
Jax I <jaxpi85@gmail.com>
2022-12-29 10:21:59 -0500

    altered layout of about me information and added techs learned

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,6 @@ const Footer = () => {
       <p>&copy; 2022 Jackson Impellizeri</p>
       <a href="https://github.com/Jaxpi">GitHub</a>
       <a href="https://linkedin.com">LinkedIn</a>
-      <a href="https://twitter.com">Twitter</a>
     </footer>
   );
 };
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,18 +1,28 @@
 const About = () => {
-    return (
-      <section className="about" id="about">
-        <h2>About Me</h2>
-        <p>
-          I am a current student of Columbia University's Columbia Engineering
-          Coding Boot Camp, where I am developing the skills and techniques to be
-          a well-rounded and knowledgeable full-stack software engineer. I
-          previously worked in the education field, but am seeking a career change
-          full of new opportunities and challenges. In my spare time I enjoy
-          traveling, hiking, music, singing, cooking, sports, films, Avatar: The
-          Last Airbender, Korean shows on Netflix, and videogames, among others.
-        </p>
-      </section>
-    );
-  };
-  
-  export default About;
+  return (
+    <section className="about" id="about">
+      <h2>About Me</h2>
+      <p>
+        I am a recent graduate of Columbia University's Columbia Engineering
+        Coding Boot Camp, where I developed the skills and techniques to be a
+        well-rounded and knowledgeable full-stack software engineer. The program
+        provided me working knowledge in a wide variety of coding technologies
+        and languages including HTML, CSS, Git, JavaScript, Bootstrap, DOM,
+        RESTful API's, JQuery, JSON, AJAX, Node.js, ES6, OOP, Express, MySQL,
+        GraphQL, NoSQL, Sequelize, Agile Development, Jest, Handlebars,
+        Progressive Web Apps, Webpacks, Vite, Apollo Client/Server, Insomnia
+        Core, VS Code, React, MERN Stacks, fundamentals of computer science, and
+        others. <br></br><br></br>
+      </p>
+      <p>
+        I previously worked in the education field, but am seeking a
+        career change full of new opportunities and challenges. In my spare time
+        I enjoy traveling, hiking, music, singing, cooking, sports, films,
+        Avatar: The Last Airbender, Korean shows on Netflix, and videogames,
+        among others.
+      </p>
+    </section>
+  );
+};
+
+export default About;
