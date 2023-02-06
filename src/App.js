6fd69bd0598ccdd518b6adc62c6b842189ef1723
Jax I <jaxpi85@gmail.com>
2023-02-06 16:50:53 -0500

    changed profile picture and altered design; replaced header content from separate component to part of about me since it is not needed on every page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Navigation from "./components/navigation";
-import Header from "./components/header";
 import Main from "./components/main"
 import Footer from "./components/footer";
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      <Header />
       <Main />
       <Footer />
     </div>
