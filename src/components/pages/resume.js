9f42cfbec4a30e9acbc4115fb0f49c7f69de3ab3
Jax I <jaxpi85@gmail.com>
2023-04-06 09:46:07 -0400

    updated resume information

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,4 +1,4 @@
-import respdf from "../../images/ImpellizeriResume.docx.pdf";
+import respdf from "../../images/ImpellizeriJResume.docx.pdf";
 
 const Resume = () => {
   return (
@@ -6,91 +6,88 @@ const Resume = () => {
       <h2>Resumé</h2>
       <ul>
         <li>
-          <h3>Employment</h3>
+          <h3>Technical Skills</h3>
           <br></br>
           <p id="resTitle">
-            Administration Intern; Science Content Team Leader; Earth
-            Science/Elective Teacher, August 2012-September 2022<br></br>
+            Languages:<br></br>
           </p>
           <p>
-            Bronx Haven High School – Bronx, NY<br></br>
-            <br></br>
-          </p>
-          <p>
-            • Administrative Intern through the College of Saint Rose CITE
-            School Building and School District Leader programs – APPR
-            evaluations; professional development; master schedule creation;
-            curriculum design/evaluation<br></br>
-          </p>
-          <p>
-            • Science Content Team Leader – Lead science department in inquiry,
-            reflection, unit and lesson plan design, and feedback for
-            improvement<br></br>
-          </p>
-          <p>
-            • Co-Leader of New Teacher Academy – Guidance, support, and
-            professional development for first year teachers <br></br>
-          </p>
-          <p>
-            • Science, art, and elective classroom teaching <br></br>
-          </p>
-          <p>
-            • Focus on data-driven teaching reflection, critical thinking,
-            problem solving, and non-cognitive learning factors <br></br>
-          </p>
-          <p>
-            • Extensive work with students with disabilities,
-            over-aged/under-credited students, and at-risk students in an urban
-            transfer high school setting
-          </p>
-          <br></br>
-          <br></br>
+            JavaScript ES6+, CSS3, HTML5, SQL, NoSQL, DOM, RESTful API’s, JSON
+          </p><br></br>
           <p id="resTitle">
-            Teaching Assistant: 1:1, January 2011-August 2012<br></br>
+            Applications:<br></br>
           </p>
           <p>
-            Leo F. Giblyn School – Freeport, NY<br></br>
-            <br></br>
+            GitHub, MongoDB, MySQL
+          </p><br></br>
+          <p id="resTitle">
+            Tools:<br></br>
           </p>
           <p>
-            • Experience with students with special needs and school-wide
-            student management
+            Express, React, Node, Handlebars, JQuery, Bootstrap, Progressive Web
+            Apps, Webpacks, Vite, Apollo Client/Server, Insomnia Core, VS Code,
+            Git, AJAX, OOP, GraphQL, Sequelize, Agile Development, Jest
+          </p><br></br>
+
+          <h3>Projects</h3>
+          <br></br>
+
+          <p id="resTitle">
+          Weather Dashboard: github.com/Jaxpi/weather-dashboard | jaxpi-weather-dashboard.herokuapp.com<br></br>
           </p>
+          <p>
+          • Application created for travelers to quickly and easily see current and near-future weather conditions for cities around the world to plan properly for their trip. Users can submit a city name to bring up the current temperature, weather, wind speed, and humidity of the location as well as the same conditions for the following 5 days. This app is also installable as a PWA.<br></br>
+          </p>
+          <p>
+          • React, JS, CSS, HTML<br></br>
+          </p>
+          <br></br>
+
+          <p id="resTitle">
+          Work Day Scheduler: github.com/Jaxpi/work-day-scheduler | jaxpi.github.io/work-day-scheduler<br></br>
+          </p>
+          <p>
+          • Application allows users to add important events to a daily planner. Events in the current hour will display red, past events are grayed-out, and future events display green for time management ease.<br></br>
+          </p>
+          <p>
+          • MomentJS, JS, CSS, HTML<br></br>
+          </p>
+          <br></br>
+
+          <p id="resTitle">
+          Jax Yoga Pop: github.com/Jaxpi/jax-yoga-pop | jaxpi.github.io/jax-yoga-pop<br></br>
+          </p>
+          <p>
+          • Bubble shooter game application that utilizes dynamic user interactions and complex logic to create a simple and enjoyable experience.<br></br>
+          </p>
+          <p>
+          • JS, CSS, HTML<br></br>
+          </p>
+          <br></br>
         </li>
         <br></br>
         <li>
+          <h3>Employment Experience</h3>
+          <br></br>
+          <p id="resTitle">Administration Intern; Science Content Team Leader; Science/Elective Teacher, August 2012-September 2023</p>
+          <p>Bronx Haven High School – Bronx, NY</p><br></br>
+          <p>• Administrative Intern – APPR evaluations; professional development; master schedule; curriculum design/evaluation</p>
+          <p>• Science Content Team Leader/Co-Leader of New Teacher Academy – Lead department and first year teachers in inquiry, reflection, unit/lesson plan design, and feedback for improvement</p>
+          <p>• Classroom teaching with focus on data-driven teaching reflection, critical thinking, problem solving, and non-cognitive learning factors with extensive work with students with disabilities, over-aged/under-credited students, and at-risk students in an urban transfer high school setting</p>
+          <br></br>
+
           <h3>Education</h3>
           <br></br>
-          <p id="resTitle">Certifications and Licenses:</p>
-          <br></br>
-          <p>Full-Stack Developer, December 2022</p>
-          <p>NYS School District Leader Certification, August 2018</p>
-          <p>NYS School Building Leader Certification, May 2018</p>
-          <p>
-            NYS Professional Certification: Earth Science (7-12), February 2011
-          </p>
-          <br></br>
-          <p id="resTitle">Educational Programs:</p>
+          <p id="resTitle">Full-Stack Coding Certificate Program, Columbia University, New York, NY</p>
+          <p id="resTitle">Advanced Certificate School District Leader, College of St. Rose – Albany, NY</p>
+          <p id="resTitle">Advanced Certificate School Building Leader, College of St. Rose – Albany, NY</p>
+          <p id="resTitle">M.A. Science Education, Adelphi University – Garden City, NY</p>
+          <p id="resTitle">B.A. Environmental Science, SUNY Purchase, Purchase, NY</p><br></br>
+
+          <p id="resTitle">Other and Related Skills:</p>
           <br></br>
           <p>
-            Full-Stack Developer Certificate, December 2022, Columbia University
-            - New York City, NY
-          </p>
-          <p>
-            Advanced Certificate School Building Leader, May 2018, College of
-            St. Rose – Albany, NY
-          </p>
-          <p>
-            Advanced Certificate School District Leader, August 2018, College of
-            St. Rose – Albany, NY
-          </p>
-          <p>
-            M.A. Science Education, GPA 3.9, January 2011, Adelphi University –
-            Garden City, NY
-          </p>
-          <p>
-            B.A. Environmental Science, January 2007, SUNY Purchase - Purchase,
-            NY
+          School District Administrator Certification, School Building Administrator Certification, Science Educator Professional Certification
           </p>
         </li>
         <br></br>
